bea01791f80a10a7e09ee26c881ec1653889647d
KungFuDonkey <sietze.riemersma@gmail.com>
2023-03-14 21:54:48 +0100

    added warmup rounds and improved logging

--- benchmarker.py
+++ benchmarker.py
@@ -1,44 +1,53 @@
-import timeit
+import random
 import time
 import tqdm
+import sys
 
-NUMBER_OF_BENCHMARKS = 100
+NUMBER_OF_BENCHMARKS_PER_INPUT = 10
+WARMUP_ROUNDS = 1000
 
 
 class BenchMarker:
 
     # takes a function as input that must be benchmarked. If you need a setup (like for unet) consider setting this
     # to a lambda function
-    def __init__(self, function, name, finalizer_function=None):
+    def __init__(self, function, name):
         self.name = name
         self.function = function
         self.averages = []
-        self.finalizer_function = finalizer_function
         self.function_result = None
 
     # Runs a full benchmark over the function, this benchmark provides inputs for the function.
     # in our case the inputs can be filled in with opencv images
     def run_full_benchmark(self, inputs):
+        start_time = time.time()
         print('running benchmarker {name}'.format(name=self.name))
+        print('warming up')
+        for x in tqdm.tqdm(range(WARMUP_ROUNDS), file=sys.stdout):
+            self.function(random.choice(inputs))
 
-        # tqdm is for a progressbar in the console, iterates over inputs
-        for i in tqdm.tqdm(range(len(inputs))):
+        print('performing benchmark')
+        for i in tqdm.tqdm(range(len(inputs)), file=sys.stdout):
             self.run_benchmark_with_input(inputs[i])
 
-            # call finalizer function if available
-            if self.finalizer_function is not None:
-                self.finalizer_function(self.function_result, i)
-
-    # runs function and stores result in function_result
-    def run_function(self, function_input):
-        self.function_result = self.function(function_input)
+        print(
+            'finished benchmarker {name} in {time} seconds'.format(name=self.name, time=int(time.time() - start_time)))
 
     # Runs as benchmark over one input. It was written this way as time can differ for different inputs.
     def run_benchmark_with_input(self, function_input):
-        avg_result = timeit.Timer(lambda: self.run_function(function_input)).timeit(NUMBER_OF_BENCHMARKS)
-        self.averages += [avg_result]
+        total_time = 0
+        for i in range(1, NUMBER_OF_BENCHMARKS_PER_INPUT):
+            start_time = time.time()
+            self.function(function_input)
+            end_time = time.time()
+            total_time += end_time - start_time
 
-    # gets the results of the benchmark. Returns (avg, mean, mean_dev)
+        avg_result = total_time / NUMBER_OF_BENCHMARKS_PER_INPUT
+
+        # multiply by 1000 for MS instead of seconds
+        self.averages += [avg_result * 1000]
+
+    # gets the results of the benchmark. Returns (avg, mean, mean_dev) in MS
     def get_benchmark_results(self):
         if len(self.averages) == 0:
             return 0, 0, 0
--- main.py
+++ main.py
@@ -5,21 +5,66 @@
 import cv2 as cv
 import watershed
 import opencv_tools
-import benchmarker
+from benchmarker import BenchMarker
+import tqdm
+import sys
+from colorama import Fore
+from prettytable import PrettyTable
 
-# Press the green button in the gutter to run the script.
+
+# pretty prints the results in one table for all benches
+def pretty_print_results(results):
+    print(Fore.GREEN + 'results:' + Fore.RESET)
+    print('')
+    table = PrettyTable(['method', 'average', 'mean', 'mean_dev'])
+    for bench in results:
+        name = bench.name
+        avg, mean, mean_dev = bench.get_benchmark_results()
+        table.add_row([name, avg, mean, mean_dev])
+    print(table)
+
+
+# generates output images for this method
+def generate_output_images(method, method_name, cv_images):
+    print('generating output images for {}'.format(method_name))
+    for i in tqdm.tqdm(range(len(cv_images)), file=sys.stdout):
+        output = method(cv_images[i])
+        opencv_tools.export_image(output, method_name, str(i))
+
+
+# runs all tests for one method
+def run_tests_for_method(method, method_name, cv_images):
+    print(Fore.GREEN + 'running tests for {name}'.format(name=method_name) + Fore.RESET)
+    print('')
+
+    # run benchmark
+    bench = BenchMarker(method, method_name)
+    bench.run_full_benchmark(cv_images)
+
+    print('')
+
+    # generate output images for visualization
+    generate_output_images(method, method_name, cv_images)
+
+    print('')
+
+    # accuracy must be implemented
+
+    # return bench for pretty printing
+    return bench
+
+
+# main()
 if __name__ == '__main__':
     cv_images = opencv_tools.find_opencv_images('inputImages')
 
-    watershed_export_func = lambda image, iterator: opencv_tools.export_image(image, 'watershed', str(iterator))
-    watershed_bench = benchmarker.BenchMarker(watershed.apply_watershed, 'watershed', watershed_export_func)
+    # run multiple tests (right now only return benches)
+    test_results = \
+        [
+            run_tests_for_method(watershed.apply_watershed, 'watershed', cv_images),
+            run_tests_for_method(watershed.apply_watershed, 'watershed', cv_images)
+        ]
 
-    watershed_bench.run_full_benchmark(cv_images)
-
-    # needs a nice pretty print function for printing all the results
-    watershed_avg, watershed_mean, watershed_mean_dev = watershed_bench.get_benchmark_results()
-
-    print('avg: {avg}, mean: {mean}, mean_dev: {mean_dev}'.format(avg=watershed_avg, mean=watershed_mean,
-                                                                  mean_dev=watershed_mean_dev))
+    pretty_print_results(test_results)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
